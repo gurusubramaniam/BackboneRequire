df7a8436f61baef4099964b7a774dac089b30c1a
gurusubramaniam sundararaman <ssubramaniam@paypal.com>
2014-09-18 14:02:33 -0700

    Intiial commit with error

---
+++ js/config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+	paths : {		
+		"jquery" : "js/lib/jquery",
+		"underscore" : "js/lib/underscore",
+		"backbone": "js/lib/backbone"
+	}
+});
+
+require(['js/lib/backbone']);
---
+++ main.js
@@ -0,0 +1,3 @@
+require(['js/config'], function (config) {
+	require(['App.js']);
+});
